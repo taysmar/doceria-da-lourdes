1e61a12eaa90dac539a08875a4e7c253bcabf0b4
Maria Taysmar <taysmar@outlook.com>
2023-01-04 12:05:31 -0300

    Mudanças feitas no menu hamburguer

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-var hamburguerMenu = document.getElementById('hamburger-menu')
-var iconHamburguer = document.querySelector('img.icon-hamburguer')
-var iconCloseMenu = document.querySelector('img.icon-close')
-hamburguerMenu.addEventListener('click', clicar)
-function openMenu() {
-  //  hamburguerMenu.
-}
+const primaryNav = document.querySelector(".primary-navigation")
+const navToggle = document.querySelector(".mobile-nav-toggle")
 
-function clicar(event){
-    if (iconCloseMenu.classList.contains('hidden')){ //Se o botão de fechar estiver oculto
-        iconHamburguer.classList.add('hidden')
-        iconCloseMenu.classList.remove('hidden')
-    }
-    else {
-        iconHamburguer.classList.remove('hidden')
-        iconCloseMenu.classList.add('hidden')
-    }
-}
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible")
 
-console.log(iconCloseMenu)
+    if(visibility === "false") {
+        primaryNav.setAttribute('data-visible', true)
+        navToggle.setAttribute('aria-expanded', true)
+    } else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+    }
+})
+
+
+
+
+
+
+
+// var hamburguerMenu = document.getElementById('hamburger-menu')
+// var iconHamburguer = document.querySelector('img.icon-hamburguer')
+// var iconCloseMenu = document.querySelector('img.icon-close')
+// hamburguerMenu.addEventListener('click', clicar)
+// function openMenu() {
+//   //  hamburguerMenu.
+// }
+
+// function clicar(event){
+//     if (iconCloseMenu.classList.contains('hidden')){ //Se o botão de fechar estiver oculto
+//         iconHamburguer.classList.add('hidden')
+//         iconCloseMenu.classList.remove('hidden')
+//     }
+//     else {
+//         iconHamburguer.classList.remove('hidden')
+//         iconCloseMenu.classList.add('hidden')
+//     }
+// }
+
+// console.log(iconCloseMenu)
